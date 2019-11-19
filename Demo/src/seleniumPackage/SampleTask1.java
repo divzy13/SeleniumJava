eb68e87a1a7c24f5f7b3501c38ac5750be26d51f
divya.raveendran <divya.raveendran@SELI130583>
2019-11-19 11:28:18 +0100

    Updated with excercise class

--- Demo/src/seleniumPackage/SampleTask1.java
+++ Demo/src/seleniumPackage/SampleTask1.java
@@ -2,27 +2,26 @@ package seleniumPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class SampleTask1 {
 
+	@Test
 	public void OpenHomepage() throws Exception {
 		
 		//define browser driver in system properties
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\divv1\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
 		
-		//Try opening CGI page "https://www.cgi.com/en"
-		driver.manage().window().maximize();
-		driver.navigate().to("https://www.cgi.com/en");
+		
+		//Try opening Web page "https://www.seleniumeasy.com/test/"
+		
 		
 		//Get page title
-		System.out.println("CGI Page Title is : "+driver.getTitle());
+		
 		
 		//Get page URL
-		System.out.println("CGI Page URL is : "+driver.getCurrentUrl());
+		
 		
 		//close the browser
-		driver.quit();
 		
 		
 	}
